bf4244eb3d8a5a63f5d8b5478edc8e9361549817
kfc <kfc@example.com>
2025-06-04 10:12:17 +0900

    feature/23p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactor {
 
+    public List<Integer> of(int i) {
+        ArrayList<Integer> factors = new ArrayList<>();
+        return factors;
+    }
 }
--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorTest {
     @Test
     void testPrimeFactorOf1() {
         PrimeFactor primeFactor = new PrimeFactor();
-        assertEquals(10, 10);
+        assertEquals(Arrays.asList(),primeFactor.of(1));
     }
 }
