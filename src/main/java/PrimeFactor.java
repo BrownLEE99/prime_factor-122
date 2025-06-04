a7bf594a0b9a71e44de8d17e69020b5263586270
kfc <kfc@example.com>
2025-06-04 10:16:51 +0900

    feature/35p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -7,6 +7,8 @@ public class PrimeFactor {
         ArrayList<Integer> factors = new ArrayList<>();
         if(number == 2) {
             factors.add(2);
+        } else if(number == 3) {
+            factors.add(3);
         }
         return factors;
     }
